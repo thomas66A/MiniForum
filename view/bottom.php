3ebfb55c683b911efa862f693b8466303e36aade
thomas66A <thomas.ohare@laposte.net>
2017-07-07 17:03:19 +0200

    deuxieme commit

---
+++ view/bottom.php
@@ -0,0 +1,5 @@
+<script src="JsScript/jquery-3.2.1.min.js"></script>
+<script src="JsScript/color.js"></script> 
+<script src="JsScript/script.js"></script>
+</body>
+</html>
---
+++ view/top.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="styles/css/reset.css">
+    <link rel="stylesheet" type="text/css" href="styles/css/styles.css">
+    <title>Colors, le forum,dédié à la couleur</title>
+</head>
+<body>    
+</body>
+</html>
+<div id="top">
+    <img src="image/roue.png">
+    <h1>Le forum consacré à la couleur</h1>
+</div>
