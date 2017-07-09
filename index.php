0327ca55cac5ef2306806659ed81f021163c0e87
Alfonso Fernandez <le_debutant@yahoo.fr>
2017-07-09 10:39:34 +0200

    rajouté des commentaires dans index.php(le controlleur) et dans service.php

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 include('view/top.php');
 include('view/bottom.php');
 
+
+// Alfonso:c'est par ici qu'il faut commencer
+// Il faut reprendre ce que j'ai fait dans le dossier
+// MVC et faire le switch avant de balancer les vues
+// Je pense qu'il faut rajouter le header et le footer
+// dans les vues plutot qu'ici dans index.php (le controlleur)
+// On en rediscute. T'es arrivé un peu en retard et tu
+// n'as pas eu le temps de bien le voir je pense.
+
 ?>
--- service/service.php
+++ service/service.php
@@ -1,4 +1,11 @@
 <?php
+
+// Alfonso: Les services ne sont pas sensées avoir de
+// fonctions. Il faut mettre ça dans model même si c'est
+// des fonctions de checking. Tu peux aussi au lieu de le
+// mettre dans fonction le mettre dans un autre fichier
+// checkingModel.php ou quelque chose comme ça
+
 function voirMajeur($age)
 {
     if($age<18)
